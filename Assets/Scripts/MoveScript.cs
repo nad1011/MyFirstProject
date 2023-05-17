f2cfcb581281f9da1ff96467e3028682999f56ba
Haidan1011 <101380985+Haidan1011@users.noreply.github.com>
2023-05-17 15:06:13 +0700

    Initial commit

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 20; 
+        int b = 20;
+        Debug.Log(a + " + " + b + (a + b));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
